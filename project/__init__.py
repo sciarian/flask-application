1b39fb9ce755138ad0cf4e4814daba0aa633d0b4
Anthony Sciarini <sciarian@mail.gvsu.edu>
2018-08-15 08:33:55 -0400

    Added base.html

--- project/__init__.py
+++ project/__init__.py
@@ -19,10 +19,16 @@ from flask import Flask
 #to what secret key is used in the configuration file.
 
 app = Flask(__name__, instance_relative_config=True)
-    #Few things to take note of, __name__ refers to the module that the code is being used from.
-    #if it is from the source code of the python code being run then it is the called '__main__'
+    #Few things to take note of, __name__ refers to the module that the code is
+    #being used from.if it is from the source code of the python code being run 
+    #then it is the called '__main__'. In this case the flask application is called project.
 
+#Sets the configuration file for the flask application.
 app.config.from_pyfile('flask.cfg')
+    #Tells the flask application that it should refer to the given file name 
+    #(flask.cfg in this case) in the instance_path folder for loading
+    #application confurations rather than the root directory of the project.
 
+#TODO what does this mean???
 from . import views
-
+    #This imports everything in the views dir, but why?
--- project/views.py
+++ project/views.py
@@ -1,15 +1,17 @@
 ###
+#
 # This file is used to store initial routes used by the web application.
 # It can show the address of the page being used for easy readability.
-# 
+#
+##
 
 #Import instnce of the flask application
 from . import app
-from flask import render_template
+from flask import render_template       #Import flask object to render a webpage template.
 
 #This functions goes to the base adress of the main address of the website
 @app.route('/')
 def index():
     #To send html docs in templates dir use "render_template('doc_name.html')"
-    return render_template('test.html')
+    return render_template('base.html')
 
