2cf22b95b252c4f3e98036e53b49b60a8bbb5aa5
Anthony Sciarini <sciarian@mail.gvsu.edu>
2018-08-23 12:16:12 -0400

    Added links to all academic schedules

--- project/schedule/views.py
+++ project/schedule/views.py
@@ -28,16 +28,37 @@ from project.models import Course
 ### Config ###
 ##############
 
-schedule_blueprint = Blueprint('schedule', __name__, template_folder='templates')
+sr_schedule_blueprint = Blueprint('sr_schedule', __name__, template_folder='templates')
+jr_schedule_blueprint = Blueprint('jr_schedule', __name__, template_folder='templates')
+sp_schedule_blueprint = Blueprint('sp_schedule', __name__, template_folder='templates')
+fr_schedule_blueprint = Blueprint('fr_schedule', __name__, template_folder='templates')
 
 ##############
 ### Routes ###
 ##############
 
 #This functions goes to the base adress of the main address of the website.
-@schedule_blueprint.route('/')
+@sr_schedule_blueprint.route('/')
 def index():
     #To send html docs in templates dir use "render_template('doc_name.html')"
     #To grab all of the data from the database use ModelName.query.all()
     all_courses = Course.query.all()
-    return render_template('schedule.html', courses = all_courses)
+    return render_template('sr_schedule.html', courses = all_courses)
+
+#This function will have the route that returns the junior schedule html page 
+@jr_schedule_blueprint.route('/jr_schedule')
+def jr_sched_index():
+    all_courses = Course.query.all()                                 #Query all classes
+    return render_template('jr_schedule.html', courses = all_courses)#Return template
+
+#This function will have the route that returns the sophmo schedule html page
+@sp_schedule_blueprint.route('/sp_schedule')
+def sp_sched_index():
+    all_courses = Course.query.all()                                 #Query all classes
+    return render_template('sp_schedule.html', courses = all_courses)#Return template
+
+#This function will have the route that returns the freshm schedule html page
+@fr_schedule_blueprint.route('/fr_schedule')
+def fr_sched_index():
+    all_courses = Course.query.all()                                 #Query all classes
+    return render_template('fr_schedule.html', courses = all_courses)#Return template
